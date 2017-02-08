3e0bc49aea74f85b439d5308e874a1ea39f604fd
rails projects <maosheng421@gmail.com>
2017-02-08 14:24:13 +0000

    change topic of website

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     root 'pages#homepage'
     get 'about', to: 'pages#about'
     
-    resource :articles
+    resources :articles
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
